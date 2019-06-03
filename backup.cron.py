1b9943241533af4f2653a664dffd4165137cad3b
David Scheuneman <35669036+dscheuneman@users.noreply.github.com>
2019-06-03 15:55:43 +0200

    added pruning for incremental backup files

--- backup.cron.py
+++ backup.cron.py
@@ -2,6 +2,7 @@
 from subprocess import call
 from operator import itemgetter, attrgetter
 import glob, os, smtplib
+
 #set empty global string to contain error messages
 notify=""
 
@@ -15,7 +16,7 @@ def main():
     except:
         notify+="S3 sync failed\n"
     else:
-        prune_old_backups(5)
+        prune_old_backups(3)
 
     #send email if there's anything to notify
     if (notify):
@@ -32,40 +33,40 @@ def sort_files_by_last_modified(files):
     return fileData 
 
 def delete_oldest_files(sorted_files, keep = 3):
+    global notify
     """ Given a list of files sorted by last modified time and a number to 
         keep, delete the oldest ones. """
     delete = len(sorted_files) - keep
     for x in range(0, delete):
-        print "Deleting: " + sorted_files[x][0]
+        notify += "Deleting: " + sorted_files[x][0] +"\n"
         os.remove(sorted_files[x][0])
 
 # sync backups to s3
 def sync_s3():
     copybackups = call (['/usr/bin/aws', 's3', 'sync', '/home/existdb/exist-backup', 's3://us-east-1-84000.co-backup/84000-translate.org/eXist-backup'])
     copyresources = call (['/usr/bin/aws', 's3', 'sync', '/var/www/html/translator-resources', 's3://us-east-1-84000.co-backup/84000-translate.org/translator-resources'])
+    copyxlogs = call (['/usr/bin/aws', 's3', 'sync', '/home/existdb/exist-xml-logs', 's3://us-east-1-84000.co-backup/84000-translate.org/eXist-xml-logs'])
     """non-zero above means there was an error"""
     """return true if success, false if error"""
-    return not(bool(copybackups or copyresources))
+    return not(bool(copybackups or copyresources or copyxlogs))
 
 
 # keep X number of backup and log files, delete others
 def prune_old_backups(keep):
-    # Find all backup files matching the path.
+    # Find & sort full-backup files matching the path. Delete older than "keep" number of files.
     file_paths = glob.glob('/home/existdb/exist-backup/full*.zip')
-
-    # Sort the files according to the last modification time.
     sorted_files = sort_files_by_last_modified(file_paths)
-
     delete_oldest_files(sorted_files, keep)
 
-
-    # Find all log files matching the path.
+    #Same for log files 
     file_paths = glob.glob('/home/existdb/exist-backup/report*.log')
-
-    # Sort the files according to the last modification time.
     sorted_files = sort_files_by_last_modified(file_paths)
-
     delete_oldest_files(sorted_files, keep)
+
+    #Same for incremental backups, but keep 7.
+    file_paths = glob.glob('/home/existdb/exist-backup/inc*.zip')
+    sorted_files = sort_files_by_last_modified(file_paths)
+    delete_oldest_files(sorted_files, 7)
 
 def check_backup_log():
     global notify
